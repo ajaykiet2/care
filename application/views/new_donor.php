ff32c02ebfffac3a13aae10f94fcd23f077f5915
Ajay Kumar <ajay.kumar@imaginea.com>
2019-03-12 02:38:35 +0530

    Add Donor form added

--- application/views/new_donor.php
+++ application/views/new_donor.php
@@ -2,94 +2,126 @@
 <?php $this->load->view("common/header");?>
 <?php $this->load->view("common/widgets/blank_header");?>
 <div class="content">
-<div class="col-md-10 mr-auto ml-auto">
-    <div class="wizard-container" id="newDonee">
-        <div class="card card-wizard active" data-color="primary">
-            <div class="card-header text-center" data-background-color="black">
-                <h3 class="card-title text-left">ADD NEW DONEE</h3>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="input-group form-control-lg">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">
-                                    <i class="now-ui-icons users_circle-08"></i>
-                                </div>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card ">
+                <div class="card-header ">
+                    <h4 class="card-title">New Donor Profile Details</h4>
+                </div>
+                <div class="card-body ">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <label>Name</label>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="name">
                             </div>
-                            <input type="text" id="name" class="form-control" placeholder="Name (Required)" name="name">
                         </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="input-group form-control-lg">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">
-                                    <i class="now-ui-icons text_caps-small"></i>
-                                </div>
+                        <div class="col-sm-6">
+                            <label>Mobile</label>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="mobile">
                             </div>
-                            <input type="text" id="username" placeholder="Username (Required)" class="form-control" name="username"/>
                         </div>
-                    </div>
-                    <div class="col-sm-5">
-                        <div class="input-group form-control-lg">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">
-                                    <i class="now-ui-icons text_caps-small"></i>
-                                </div>
+                        <div class="col-sm-12">
+                            <label>Email ID</label>
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="email">
                             </div>
-                            <input type="text" id="mobile" placeholder="Mobile (Required)" class="form-control valid-mobile" name="mobile"/>
                         </div>
-                    </div>
-                    <div class="col-sm-7">
-                        <div class="input-group form-control-lg">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">
-                                    <i class="now-ui-icons text_caps-small"></i>
-                                </div>
+                        <div class="col-sm-12">
+                            <label>Address</label>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="address">
                             </div>
-                            <input type="email" id="email" placeholder="Email (Required)" class="form-control" name="email"/>
                         </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="input-group form-control-lg">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">
-                                    <i class="now-ui-icons users_circle-08"></i>
-                                </div>
+                        <div class="col-sm-6">
+                            <label>Latitude</label>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="latitude">
                             </div>
-                            <input type="password" id="password" class="form-control" placeholder="Password (Required)" name="password" id="password">
                         </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="input-group form-control-lg">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">
-                                    <i class="now-ui-icons text_caps-small"></i>
-                                </div>
+                        <div class="col-sm-6">
+                            <label>Longitude</label>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="longitude">
                             </div>
-                            <input type="password" id="re_password" placeholder="Re-Password (Required)" class="form-control" name="re_password"/>
-                        </div>
-                    </div>
-                    <div class="col-lg-12">
-                        <div class="form-group">
-                            <input type="text" id="address" name="address" class="form-control" placeholder="Donee Address">
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="card-footer">
-                <div class="row"> 
-                    <div class="col-sm-12">
-                        <div class="pull-right">
-                            <span class="btn btn-danger btn-round" id="clearForm"><i class="fa fa-refresh"></i> Clear</span>
-                            <span class="btn btn-primary btn-round" id="addNewDonee"><i class="fa fa-hdd"></i> Save</span>
-                        </div>  
-                    </div>               
+        </div>
+        <div class="col-sm-6">
+            <div class="card ">
+                <div class="card-header ">
+                    <h4 class="card-title">New Donor Other Information</h4>
+                </div>
+                <div class="card-body ">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <label>Aadhaar Number</label>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="aadhaar_number">
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <label>PAN Number</label>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="pan_number">
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div style="height:10px;width:100%"></div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label>Payment Mode</label>
+                                <div>
+                                    <select class="selectpicker" id="payment_mode" data-size="2" data-style="form-control btn btn-primary btn-round btn-lg" title="Payment Mode">
+                                        <option value="online" selected>Online</option>
+                                        <option value="offline">Offline</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label>Account Status</label>
+                                <div>
+                                    <select class="selectpicker" id="status" data-size="2" data-style="form-control btn btn-primary btn-round btn-lg" title="Account Status">
+                                        <option value="active" selected>Active</option>
+                                        <option value="inactive">Inactive</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div style="height:10px;width:100%"></div>
+                        <div class="col-sm-4">
+                            <label>Amount</label>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="amount">
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <label>Latitude</label>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="latitude">
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <label>Longitude</label>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="longitude">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer text-right">
+                    <button class="btn btn-fill btn-default">Clear Form</button>
+                    <button class="btn btn-fill btn-primary">Add Donor</button>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </div>
 <?php $this->load->view("common/scripts");?>
 <script src="<?=base_url("assets/js/plugins/bootstrap-selectpicker.js")?>"></script>
